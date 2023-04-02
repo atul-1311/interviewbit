a88df7f8ed5b216d7415f7d39703fb7ed0ec6f09
Atul Ranjan <75110045+atul-1311@users.noreply.github.com>
2023-04-02 18:01:19 +0530

    Update email-service.js

--- backend/services/email-service.js
+++ backend/services/email-service.js
@@ -26,27 +26,3 @@ class EmailService{
 }
 
 module.exports = new EmailService();
-
-// const nodemailer = require('nodemailer');
-// async function sendMail ({ from, sendername, to, subject, text, html }) {
-//     let transporter = nodemailer.createTransport({
-//         host: process.env.SMTP_HOST,
-//         port: process.env.SMTP_PORT, 
-//         secure: false,
-//         auth: {
-//             user: process.env.MAIL_USER,
-//             pass: process.env.MAIL_PASS
-//         }
-//     });
-
-//     let info = await transporter.sendMail({
-//         from: `SenDoc <${from}>`,
-//         sendername: sendername,
-//         to: to,
-//         subject: subject,
-//         text: text,
-//         html: html
-//     });
-// }
-
-// module.exports = sendMail;
